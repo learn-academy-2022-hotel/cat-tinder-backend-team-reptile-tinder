b6ff07adca3a1d9e814f1a3589a10007ad6f73ec
James Quillen <james.quillen@gmail.com>
2023-01-31 10:26:37 -0800

    Completed index and create endpoints

--- app/controllers/reptiles_controller.rb
+++ app/controllers/reptiles_controller.rb
@@ -1,2 +1,27 @@
 class ReptilesController < ApplicationController
+
+    def index
+        reptiles = Reptile.all
+        render json: reptiles
+    end
+
+    def create
+        reptile = Reptile.create(reptile_params)
+        render json: reptile
+    end
+
+    def update
+        reptile = Reptile.find(params[:id])
+        reptile.update(reptile_params)
+        render json: reptile
+    end
+
+    def destroy
+    end
+    
+    private
+    def reptile_params
+        params.require(:reptile).permit(:name, :age, :enjoys, :image)
+    end
+
 end
--- spec/requests/reptiles_spec.rb
+++ spec/requests/reptiles_spec.rb
@@ -2,6 +2,75 @@ require 'rails_helper'
 
 RSpec.describe "Reptiles", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list off all the reptiles" do
+      Reptile.create(
+        name: 'Shakespeare',
+        age: 7,
+        enjoys: 'Eating crickets, a lot. Being pet on his little head. Adventuring',
+        image: 'https://images.unsplash.com/photo-1619153832248-6458a856d91f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80'
+      )
+      
+      get '/reptiles'
+
+      reptile = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(reptile.length).to eq 1
+    end
   end
+  
+  
+  describe "POST /create" do
+    it "creates a reptile" do
+      reptile_params = {
+      reptile: {
+        name: 'Shakespeare',
+        age: 7,
+        enjoys: 'Eating crickets, a lot. Being pet on his little head. Adventuring',
+        image: 'https://images.unsplash.com/photo-1619153832248-6458a856d91f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80'
+      }
+    }
+      
+      post  '/reptiles', params: reptile_params
+
+      expect(response).to have_http_status(200)
+  
+      reptile = Reptile.first
+
+      expect(reptile.name).to eq 'Shakespeare'
+    end
+  end
+  
+  # describe "PATCH /update" do
+  #   it "updates a reptile" do
+  #     Reptile.create(
+  #       name: 'Steve',
+  #       age: 7,
+  #       enjoys: 'Eating crickets, a lot. Being pet on his little head. Adventuring',
+  #       image: 'https://images.unsplash.com/photo-1619153832248-6458a856d91f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80'
+  #     )
+  #     reptile_params = {
+  #     reptile: {
+  #       name: 'Shakespeare',
+  #       age: 7,
+  #       enjoys: 'Eating crickets, a lot. Being pet on his little head. Adventuring',
+  #       image: 'https://images.unsplash.com/photo-1619153832248-6458a856d91f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80'
+  #     }
+  #   }
+      
+  #     patch  '/reptiles/', params: reptile_params
+
+  #     expect(response).to have_http_status(200)
+  
+  #     reptile = Reptile.first
+
+  #     expect(reptile.name).to eq 'Shakespeare'
+  #   end
+  # end
+
+
+
 end
+
+
+
+
